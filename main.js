8f260a5b9d26634dbc779c3fc5b554fdecade954
Pavlo Andrusiak <Pavlo Andrusiak>
2016-11-29 14:54:06 +0200

    refactoring: 2 spaces instead of 4

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
  */
 var metasync = require('./metasync');
 dataCollectorTimeout = new metasync.DataCollectorTimeout(4, function(err, data){
-    if (!err) console.log('data received'); else console.log(err.message);
+  if (!err) console.log('data received'); else console.log(err.message);
 },10);
 dataCollectorTimeout.collect(1, "some data1");
 dataCollectorTimeout.collect(2, "some data2");
 
 setTimeout(function () {
-    dataCollectorTimeout.collect(3, "some data3");
+  dataCollectorTimeout.collect(3, "some data3");
 },5000);
 dataCollectorTimeout.collect(4, "some data4");
